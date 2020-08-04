d589a35701da78c9f73d43d60c133aa61dd074f8
Michael Rabon <nobar86@gmail.com>
2020-08-04 20:54:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,8 +28,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
- shipping_manifest[muskets] => "2"
- shipping_manifest[gun powder] => "4"
+ shipping_manifest["muskets"] = 2
+ shipping_manifest["gun powder"] = 4
  shipping_manifest
   # return the shipping_manifest hash below
 
